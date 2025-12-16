28ba9de37bb19816234140227d3dbcf7fecc0e1b
Jeff Holman <jholman@orionhealthcare.onmicrosoft.com>
2025-12-16 13:37:39 -0500

    Connect the frontend and backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -8,8 +8,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  //res.send("Hello Jeff!");
   res.send("Hello world");
+});
+
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.send("Hi Jeff!");
 });
 
 app.listen(port, () => {
